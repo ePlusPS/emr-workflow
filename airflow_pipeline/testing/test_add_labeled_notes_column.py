0aaacdf4bac7df471830cfb0c304eded7e379d45
ubuntu <morgan.worthington@eplus.com>
2020-08-04 18:26:33 -0700

    added test cases for add_labeled_notes_column and create_lda_model

--- airflow_pipeline/testing/test_add_labeled_notes_column.py
+++ airflow_pipeline/testing/test_add_labeled_notes_column.py
@@ -3,12 +3,49 @@ sys.path.insert(0,'..')
 
 import unittest
 import add_labeled_notes_column
+import pandas as pd
 
 class SomeCallableTest(unittest.TestCase):
 
-    def test_1(self):
-        #assert(somecallable.some_function() == 'some expected value')
-        assert(2 == 2)
+    # tests for get_line_length_array
+    def test_get_length_array_multiple_notes_varying_lengths(self):
+        df = pd.DataFrame()
+        row1 = {'ner_cleaned_notes': 'The quick brown fox \n jumped over the \n log'}
+        row2 = {'ner_cleaned_notes': 'test test 12 check'}
+        row3 = {'ner_cleaned_notes': 'Down by the bay \n Where the watermelons grow'}
+        df = df.append(row1, ignore_index=True)
+        df = df.append(row2, ignore_index=True)
+        df = df.append(row3, ignore_index=True)
+        
+        length_array = add_labeled_notes_column.get_line_length_array(df)
+        expected_array = [3,1,2]
+
+        assert(length_array == expected_array)
+
+    # tests for get_note_lines_from_file
+    def test_get_note_lines_from_files_standard(self):
+        lines = add_labeled_notes_column.get_note_lines_from_file()
+        expected = ['The quick brown fox \n',' jumped over the \n', ' log\n', 'test test 12 check\n', 'Down by the bay \n', ' Where the watermelons grow\n']
+        assert(lines == expected)
+
+    # tests for make_column
+    def test_make_column_no_entities(self):
+        df = pd.DataFrame()
+        row1 = {'ner_cleaned_notes': 'The quick brown fox \n jumped over the \n log\n'}
+        row2 = {'ner_cleaned_notes': 'test test 12 check\n'}
+        row3 = {'ner_cleaned_notes': 'Down by the bay \n Where the watermelons grow\n'}
+        df = df.append(row1, ignore_index=True)
+        df = df.append(row2, ignore_index=True)
+        df = df.append(row3, ignore_index=True)
+        
+        length_array = [3, 1, 2]
+        lines = ['The quick brown fox \n',' jumped over the \n', ' log\n','test test 12 check\n','Down by the bay \n',' Where the watermelons grow\n']
+        
+        #Since no entities are present and the notes are both cleaned the same way, the columns for ner_cleaned_notes and the ner_labeled_notes should be the same
+        column = add_labeled_notes_column.make_column(lines, length_array)
+        expected = df['ner_cleaned_notes'].to_list()
+        assert(column == expected)
+
 
 if __name__ == '__main__':
     unittest.main()
--- airflow_pipeline/testing/test_create_lda_model.py
+++ airflow_pipeline/testing/test_create_lda_model.py
@@ -5,10 +5,29 @@ import unittest
 import create_lda_model
 
 class SomeCallableTest(unittest.TestCase):
+    
+    # tests for generate_ngrams
+    def test_generate_ngrams_standard(self):
+        test_string = 'The quick brown fox jumped over the log. It was a hot day.'
+        n = 5
+        ngrams = create_lda_model.generate_ngrams(test_string, n)
+        expected = ['the_quick_brown_fox_jumped', 'quick_brown_fox_jumped_over', 'brown_fox_jumped_over_the', 'fox_jumped_over_the_log.', 'jumped_over_the_log._was', 'over_the_log._was_hot', 'the_log._was_hot_day.']
+        assert(ngrams == expected)
 
-    def test_1(self):
-        #assert(somecallable.some_function() == 'some expected value')
-        assert(2 == 2)
+    # tests for create_ngram_tokens
+    def test_create_ngram_tokens_standard(self):
+        test_text = 'The quick brown fox jumped over the log. It was a hot day. This is a test sentence with a lot of words.'
+        ngram_tokens = create_lda_model.create_ngram_tokens(test_text)
+        expected = ['the_quick_brown_fox_jumped','quick_brown_fox_jumped_over','brown_fox_jumped_over_the','fox_jumped_over_the_log.','was_hot_day.','this_test_sentence_with_lot','test_sentence_with_lot_words.']
+        print(ngram_tokens)
+        print(expected)
+        assert(ngram_tokens==expected)
+
+    # tests for make_model
+    def test_make_model_check_non_null_output(self):
+        tokens = ['the_quick_brown_fox_jumped','quick_brown_fox_jumped_over','brown_fox_jumped_over_the','fox_jumped_over_the_log.','was_hot_day.','this_test_sentence_with_lot','test_sentence_with_lot_words.']
+        dictionary, corpus, lda_model = create_lda_model.make_model(tokens)
+        assert(dictionary is not None and corpus is not None and lda_model is not None)
 
 if __name__ == '__main__':
     unittest.main()
