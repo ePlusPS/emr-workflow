28c6bd7bd6b6b1f94430ae998d5b4a717ace06c5
ubuntu <morgan.worthington@eplus.com>
2020-08-09 01:29:23 -0700

    added two more test cases for patient_summary_df

--- airflow_pipeline/testing/test_create_report_summary.py
+++ airflow_pipeline/testing/test_create_report_summary.py
@@ -45,6 +45,87 @@ class SomeCallableTest(unittest.TestCase):
 
         assert(missing == 0)
 
+    def test_make_patient_summary_two_entries_one_patient(self):
+        df = pd.DataFrame()
+        row1 = {
+                'patient_id': 1,
+                'readmission':0,
+                'admittime': 'Mon, 07 May 2131 00:00:00 GMT',
+                'icd_codes': [5,8,72],
+                'los': 5,
+                'age': 32,
+                'gender': 'M',
+                'insurance':'MEDICAID',
+                'diagnosis':'heart attack'
+               }
+        row2 = {
+                'patient_id': 1,
+                'readmission':1,
+                'admittime': 'Mon, 14 May 2131 00:00:00 GMT',
+                'icd_codes': [5,35,72],
+                'los': 3,
+                'age': 32,
+                'gender': 'M',
+                'insurance':'MEDICAID',
+                'diagnosis':'heart attack'
+               }
+        df = df.append(row1, ignore_index = True)
+        df = df.append(row2, ignore_index = True)
+
+        summary_df = create_report_summary.make_patient_summary(df)
+
+        # check if los_avg and icd_codes columns are combined from the two separate entries in df
+
+        expected_los_avg = 4
+        expected_icd_codes = [5, 8, 72, 35]
+
+        actual_los_avg = summary_df['los_avg'][0]
+        actual_icd_codes = summary_df['icd_codes'][0]
+
+        assert(actual_los_avg == expected_los_avg and actual_icd_codes == expected_icd_codes)
+
+    def test_make_patient_summary_multiple_patients(self):
+        df = pd.DataFrame()
+        row1 = {
+                'patient_id': 1,
+                'readmission':1,
+                'admittime': 'Mon, 07 May 2131 00:00:00 GMT',
+                'icd_codes': [5,8,72],
+                'los': 5,
+                'age': 32,
+                'gender': 'M',
+                'insurance':'MEDICAID',
+                'diagnosis':'heart attack'
+               }
+        row2 = {
+                'patient_id': 2,
+                'readmission':1,
+                'admittime': 'Mon, 07 May 2131 00:00:00 GMT',
+                'icd_codes': [9,20],
+                'los': 3,
+                'age': 25,
+                'gender': 'F',
+                'insurance':'UNINSURED',
+                'diagnosis':'panic attacks'
+               }
+        df = df.append(row1, ignore_index = True)
+        df = df.append(row2, ignore_index = True)
+
+        summary_df = create_report_summary.make_patient_summary(df)
+
+        #figure out some stuff
+        expected_icd_codes_1 = [5,8,72]
+        expected_icd_codes_2 = [9,20]
+        expected_los_avg_1 = 5
+        expected_los_avg_2 = 3
+
+        actual_icd_codes_1 = summary_df['icd_codes'][0]
+        actual_icd_codes_2 = summary_df['icd_codes'][1]
+        actual_los_avg_1 = summary_df['los_avg'][0]
+        actual_los_avg_2 = summary_df['los_avg'][1]
+
+        assert(actual_icd_codes_1 == expected_icd_codes_1 and actual_icd_codes_2 == expected_icd_codes_2 and actual_los_avg_1 == expected_los_avg_1 and actual_los_avg_2 == expected_los_avg_2)
+
     # tests for make_hospital_summary
     def test_make_hospital_summary_check_columns(self):
         df = pd.DataFrame()
@@ -119,11 +200,6 @@ class SomeCallableTest(unittest.TestCase):
                 missing += 1
 
         assert(missing == 0)
-
-
-        
-
-
 
 if __name__ == '__main__':
     unittest.main()
