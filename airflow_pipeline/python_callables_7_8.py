a6a1391afe4565828b6865486eaed5f7c59101b4
ubuntu <morgan.worthington@eplus.com>
2020-08-13 14:30:48 -0700

    added callables to integration testing script

--- airflow_pipeline/python_callables_7_8.py
+++ airflow_pipeline/python_callables_7_8.py
@@ -3,6 +3,8 @@ from fe_from_structured_readmit_los import create_structured_data_features
 from word2vec_prep_clean_notes import clean_all_notes as word2vec_all_clean_notes
 from word2vec_prep_tokenize_notes import tokenize_all_notes as word2vec_all_tokenize_notes
 from create_word2vec_model import create_word2vec_model as word2vec_all_create_model
+from create_lda_model import create_lda_model
+from lda_topics_per_note import get_ngrams_per_note
 from fe_from_infection_keywords import infected_one_hot
 from readmission_word2vec_prep_clean_notes import clean_readmission_notes
 from readmission_word2vec_prep_tokenize_notes import tokenize_readmission_notes
@@ -19,72 +21,82 @@ from xgb_los_feature_entities import make_predictions as make_predictions_los_fe
 from xgb_los_medication_entities import make_predictions as make_predictions_los_med
 from xgb_los_neg_feature_entities import make_predictions as make_predictions_los_neg_feat
 from xgb_los_neg_medication_entities import make_predictions as make_predictions_los_neg_med
+from xgb_los_lda_topics import make_predictions as make_predictions_los_lda_topics
 from xgb_readmission_demographics import make_predictions as make_predictions_readm_demographics
 from xgb_readmission_feature_entities import make_predictions as make_predictions_readm_feat
 from xgb_readmission_medication_entities import make_predictions as make_predictions_readm_med
 from xgb_readmission_neg_feature_entities import make_predictions as make_predictions_readm_neg_feat
 from xgb_readmission_neg_medication_entities import make_predictions as make_predictions_readm_neg_med
+from xgb_readmission_lda_topics import make_predictions as make_predictions_readm_lda_topics
 from combine_los_estimates_tensorflow import make_predictions as make_predictions_tensorflow_los
 from combine_readmission_probabilities_tensorflow import make_predictions as make_predictions_tensorflow_readm
 from combine_los_estimates_tensorflow import make_predictions as make_predictions_tensorflow_los
 from readmission_tf_prob_to_likert import convert_to_likert
-from create_summary_report import create_report
+from create_report_summary import create_report
 
 #get_dataframe_from_apis()
 #print('1. Done creating dataframe')
-create_structured_data_features()
-print('2. Done creating structured data features')
+#create_structured_data_features()
+#print('2. Done creating structured data features')
 word2vec_all_clean_notes()
 print('3. Done cleaning notes word2vec all')
 word2vec_all_tokenize_notes()
 print('4. Done tokenizing notes word2vec all')
 word2vec_all_create_model()
 print('5. Done creating model word2vec all')
+create_lda_model()
+print('6. Done creating LDA model')
+get_ngrams_per_note()
+print('7. Done getting ngrams per note')
 infected_one_hot()
-print('6. Done one-hot-encoding infected keywords with word2vec all model')
+print('8. Done one-hot-encoding infected keywords with word2vec all model')
 clean_readmission_notes()
-print('7. Done cleaning notes word2vec readmission')
+print('9. Done cleaning notes word2vec readmission')
 tokenize_readmission_notes()
-print('8. Done tokenizing notes word2vec readmission')
+print('10. Done tokenizing notes word2vec readmission')
 word2vec_readmission_create_model()
-print('9. Done creating model word2vec readmission')
+print('11. Done creating model word2vec readmission')
 readmission_one_hot()
-print('10. Done one-hot-encoding readmission keywords with word2vec readmission model')
+print('12. Done one-hot-encoding readmission keywords with word2vec readmission model')
 clean_ner_notes()
-print('11. Done cleaning notes NER')
+print('13. Done cleaning notes NER')
 create_file()
-print('12. Done creating all note lines file for NER processing')
+print('14. Done creating all note lines file for NER processing')
 label_notes()
-print('13. Done labeling notes with NER Model')
+print('15. Done labeling notes with NER Model')
 create_labeled_notes_column()
-print('14. Done creating labeled_notes_column')
+print('16. Done creating labeled_notes_column')
 create_entity_columns()
-print('15. Done creating entity columns from the NER-labeled notes')
-make_predictions_los_demographics()
-print('16. Done creating xgboost model for los demographics')
+print('17. Done creating entity columns from the NER-labeled notes')
+#make_predictions_los_demographics()
+#print('18. Done creating xgboost model for los demographics')
 make_predictions_los_feat()
-print('17. Done creating xgboost model for los feature entities')
+print('18. Done creating xgboost model for los feature entities')
 make_predictions_los_med()
-print('18. Done creating xgboost model for los medication entities')
+print('19. Done creating xgboost model for los medication entities')
 make_predictions_los_neg_feat()
-print('19. Done creating xgboost model for los negation feature entities')
+print('20. Done creating xgboost model for los negation feature entities')
 make_predictions_los_neg_med()
-print('20. Done creating xgboost model for los negation medication entities')
+print('21. Done creating xgboost model for los negation medication entities')
+make_predictions_los_lda_topics()
+print('22. Done creating xgboost model for los lda topics')
 make_predictions_tensorflow_los()
-print('21. Done creating tensorflow model for los')
-make_predictions_readm_demographics()
-print('22. Done creating xgboost model for readmission demographics')
+print('23. Done creating tensorflow model for los')
+#make_predictions_readm_demographics()
+#print('22. Done creating xgboost model for readmission demographics')
 make_predictions_readm_feat()
-print('23. Done creating xgboost model for readmission feature entities')
+print('24. Done creating xgboost model for readmission feature entities')
 make_predictions_readm_med()
-print('24. Done creating xgboost model for readmission medication entities')
+print('25. Done creating xgboost model for readmission medication entities')
 make_predictions_readm_neg_feat()
-print('25. Done creating xgboost model for readmission negation feature entities')
+print('26. Done creating xgboost model for readmission negation feature entities')
 make_predictions_readm_neg_med()
-print('26. Done creating xgboost model for readmission negation medication entities')
+print('27. Done creating xgboost model for readmission negation medication entities')
+make_predictions_readm_lda_topics()
+print('28. Done creating xgboost model for readmission lda topics')
 make_predictions_tensorflow_readm()
-print('27. Done creating tensorflow model for readmission')
+print('29. Done creating tensorflow model for readmission')
 convert_to_likert()
-print('28. Done converting tensorflow readmission predictions to likert scale')
+print('30. Done converting tensorflow readmission predictions to likert scale')
 create_report()
-print('29. Done creating summary report')
+print('31. Done creating summary report')
